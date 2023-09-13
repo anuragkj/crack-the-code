533592929cc5714633dbcca5ebd0951c74f18725
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2023-09-13 23:10:04 +0400

    added some vault cracking progress

--- quiz/templatetags/my_custom_filters.py
+++ quiz/templatetags/my_custom_filters.py
@@ -18,3 +18,17 @@ def user_set(points):
         return math.floor(int(points)/ QUESTIONS_PER_SET) + 1
     except (ValueError, ZeroDivisionError):
         return None
+
+@register.filter
+def points_summation(obj_arr):
+    try:
+        return sum(getattr(item, "points") for item in obj_arr)
+    except:
+        return None
+
+@register.filter
+def width_giver(obj_arr, arg):
+    try:
+        return "width:" + str((int(arg)/sum(getattr(item, "points") for item in obj_arr))*100) + "%"
+    except:
+        return None
